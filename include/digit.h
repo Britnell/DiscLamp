51ab1afa8345975df0a93a35894b102b940188b9
Britnell <t.a.britnell@gmail.com>
2026-05-23 10:32:48 +0200

    4 digits pos

--- include/digit.h
+++ include/digit.h
@@ -9,7 +9,7 @@ typedef struct { int8_t r; int8_t c; } Cell;
 void draw_cells(Cell* cells, int n, int8_t ohx, int8_t ohy, uint8_t h, uint8_t s, uint8_t v) {
   for (int i = 0; i < n; i++) {
     int8_t thx = ohx + cells[i].c * 2 + (cells[i].r % 2 != 0 ? 1 : 0);
-    int8_t thy = ohy - cells[i].r;
+    int8_t thy = ohy + cells[i].r;
     for (int l = 0; l < NUM_LEDS; l++) {
       if (pixel[l].hx == thx && pixel[l].hy == thy) {
         leds[l].setHSV(h, s, v); break;
@@ -29,6 +29,16 @@ void draw8(int8_t hx, int8_t hy, uint8_t h) {
   };
   draw_cells(digit8, 10, hx, hy, h, 250, 255);
 }
+void draw2(int8_t hx, int8_t hy, uint8_t h) {
+  Cell cells[] = {
+    {0,1},{0,2},
+    {1,0},{1,2},
+    {2,1},{2,2},
+    {4,1},{4,2},
+    {3,0},{3,2},
+  };
+  draw_cells(cells, 10, hx, hy, h, 250, 255);
+}
 void draw3(int8_t hx, int8_t hy, uint8_t h) {
   Cell cells[] = { {0,1},{0,2}, {1,0}, {2,1},{2,2}, {3,0}, {4,1},{4,2} };
   draw_cells(cells, 8, hx, hy, h, 250, 255);
@@ -36,10 +46,6 @@ void draw3(int8_t hx, int8_t hy, uint8_t h) {
 void draw1(int8_t hx, int8_t hy, uint8_t h) {
   Cell cells[] = { {0,0}, {1,0}, {2,1}, {3,0}, {4,0} };
   draw_cells(cells, 5, hx, hy, h, 250, 255);
-}
-void draw2(int8_t hx, int8_t hy, uint8_t h) {
-  Cell cells[] = { {4,1},{4,0}, {3,0}, {2,1},{2,0}, {1,2}, {0,2},{0,1} };
-  draw_cells(cells, 7, hx, hy, h, 250, 255);
 }
 void draw4(int8_t hx, int8_t hy, uint8_t h) {
   Cell cells[] = { {4,2},{4,0}, {3,2},{3,0}, {2,2},{2,1},{2,0}, {1,0}, {0,0} };
@@ -55,9 +61,10 @@ static int digit_frame = 0;
 void digit_loop() {
   if (digit_frame == 0) {
     for (int l = 0; l < NUM_LEDS; l++) leds[l] = CRGB::Black;
-    draw8(-5,-1,  hue);
-    // draw8(0,0, hue);
-    // draw3(6,6,  hue);
+    draw8(-8,-6,  hue);
+    draw2(0 ,-6,  hue);
+    // draw4(-8,2,  hue);
+    // draw5(0 ,2,  hue);
     // draw4(5, 8,  hue);
     // draw5(5, 12, hue);
     FastLED.show();
